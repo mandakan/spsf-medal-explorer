bce3501cbfe44f82c03db0501ff53ff0e0aad6ec
Mathias Andersson <m@thias.se>
2025-12-22 11:41:23 +0100

    chore: remove unused file

--- src/config/siteLinks.js
+++
@@ -1,7 +0,0 @@
-export const FOOTER_INFO = {
-  author: 'Mathias A',
-  license: 'MIT',
-  github: 'https://github.com/your-username/your-repo',
-  coffee: null, // e.g. 'https://www.buymeacoffee.com/your-username' to enable
-  year: new Date().getFullYear(),
-}
