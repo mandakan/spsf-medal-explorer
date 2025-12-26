9368df5b6c0d4ea7ec7e6d9281b0b912566f4f5e
Mathias Andersson <m@thias.se>
2025-12-26 12:04:22 +0100

    chore: missing commit

--- src/contexts/medalContext.js
+++ src/contexts/medalContext.js
@@ -1,1 +1,4 @@
-export { MedalContext, MedalProvider } from './MedalContext.jsx'
+import { createContext } from 'react'
+
+export const MedalContext = createContext(null)
+export { MedalProvider } from './MedalContext.jsx'
